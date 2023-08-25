ee6b9825c9dc844dea39085a7ace9b088eddf1a6
markaeroltomarse <marktomarse@gmail.com>
2023-08-25 13:09:49 +0800

    fix: sview

--- src/layout/app-layout.layout.tsx
+++ src/layout/app-layout.layout.tsx
@@ -1,10 +1,10 @@
 import Navbar from "@components/Displays/Navbar";
 import LoadingAnimation from "@components/Displays/PageLoading";
 import ParticlesComponent from "@components/Displays/Particles";
-import useAppSelector from "@hooks/app-selector.hook";
 import useThemeStyle from "@hooks/theme-style.hook";
 import AOS from "aos";
 import "aos/dist/aos.css";
+import { useRouter } from "next/router";
 import { ReactElement, ReactNode, useEffect, useState } from "react";
 export interface AppLayoutProps {
   children: React.ReactElement;
@@ -13,7 +13,7 @@ export interface AppLayoutProps {
 
 const AppLayout: React.FC<AppLayoutProps> = (props) => {
   const { children, getLayout } = props;
-  const isDark = useAppSelector((store) => store.components.isDarkMode);
+  const router = useRouter();
   const [isLoaded, setIsLoaded] = useState(false);
 
   useEffect(() => {
@@ -28,6 +28,12 @@ const AppLayout: React.FC<AppLayoutProps> = (props) => {
     `text-slate-900 bg-slate-50`,
     `bg-slate-900 text-slate-50`
   );
+
+  useEffect(() => {
+    if (!router.query?.sview) {
+      router.push("?sview=0");
+    }
+  }, [router.query]);
 
   return (
     <div className={`${theme} min-h-[100vh] transition-all w-[100vw]`}>
